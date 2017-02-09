40e7cc0ed0a88d55ae7080523a75ab427dfb5ae5
eshenxd <xdshen@mail.ustc.edu.cn>
2017-02-09 15:56:48 +0800

    version 1.0

--- LOG/README.md
+++ LOG/README.md
@@ -2,16 +2,22 @@
 
 how to use
 ```cpp
-	#define SAVELOG  // if you want to save the log info to the file log.txt
+	#define LOGF  // if you want to save the log info to the file log.txt
 	#include "log.hpp" //include log.hpp file
 
 
 	int main()
 	{
-		LOGGER log;
-		log.LOG();
-		log.LOG("%s", "Hello world!");
-		log.LOG();
-		return 0;
+		myLog(1);
+		myLog(2);
+		myLog("test my log file");
+		myLog("%s %d %s", "test my log file", 2, "times");
 	}
 ```
+#### changes for version 1.0
+- 更改了整体架构，不再采用类的封装方式
+- 修改了log级别控制，release下不再显示调试代码是打印的位置信息
+- 不再采用默认的index，而是由每一次函数调用时通过参数index来指定
+
+#### wants to change
+- 希望在下一个版本中采用更加完整的log输出分类，能够通过log level打印不同级别的log信息
--- LOG/log.hpp
+++ LOG/log.hpp
@@ -2,20 +2,18 @@
  *	Name:log.hpp
  *	Time:2017-1-23
  *	Author:ethan
- *	Version:0.1
+ *	Version:1.0
  *	Remark:
  */
 	
 #pragma once
-#define _CRT_SECURE_NO_WARNINGS
+
 
 #ifndef __LOG__HPP__
 #define __LOG__HPP__
 
-
-
-#ifdef _DEBUG
-
+#ifdef _WIN32
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <mutex>
 #include <string>
@@ -25,67 +23,46 @@
 #include <stdarg.h>
 #include <time.h>
 
-
-
-struct LOGGER
-{
-public:
-	LOGGER();
-	~LOGGER();
-	void LOG();
-	void LOG(const char* fmt, ...);
-
-private:
-	int index;
-	
-};
-
-LOGGER::LOGGER()
-{
-	index = 1;
-
-}
-LOGGER::~LOGGER()
-{
-	index = 0;
-	
-}
-void LOGGER::LOG()
-{
-	char msg[1024];
-	sprintf_s(msg, "%s %d %s", "--------<", index, ">---------");
-	LOG("%s", msg);
-	index++;
-}
-
-void LOGGER::LOG(const char* fmt, ...)
+void myLog(const char* fmt, ...)
 {
 	FILE* f = nullptr;
-#ifdef SAVELOG
+#ifdef LOGF
 	f = fopen("log.txt", "a");
-#endif
+#endif 
 	va_list args;
 	va_start(args, fmt);
 	char msg[256];
-	vsprintf_s(msg, fmt, args);
+	vsprintf(msg, fmt, args);
 	va_end(args);
 
 	char buff[256];
 	time_t t = time(NULL);
 	strftime(buff, sizeof(buff), "[%c] ", localtime(&t));
 	char out_str[1024];
-	sprintf_s(out_str, "%s%s%s", buff, msg,"\n");
+	sprintf(out_str, "%s%s%s", buff, msg, "\n");
 	if (f)
 	{
 		fputs(out_str, f);
 		fclose(f);
 	}
 	fputs(out_str, stdout);
-	
 }
+#ifdef _DEBUG
+void myLog(int index)
+{
+	char msg[1024];
+	sprintf(msg, "%s %d %s", "--------<", index, ">---------");
+	myLog("%s", msg);
+}
+#else
+void myLog(int index)
+{
+
+}
+#endif //_DEBUG
 
 
-#endif
+#endif // _win32
 
 
 #endif //__LOG__HPP__
